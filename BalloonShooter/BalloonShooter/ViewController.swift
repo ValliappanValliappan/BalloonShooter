f946641d6fb2c257ae73e12033ec999292da10aa
ValliappanValliappan <gtvalliappan@gmail.com>
2017-11-11 21:40:11 -0800

    Revert "hope this works"
    This reverts commit fc68387258b063d80332828cf33cdeca0ee02b77.
    
    # Conflicts:
    #
    BalloonShooter/BalloonShooter.xcodeproj/project.xcworkspace/xcuserdata/n
    ipunchhajed.xcuserdatad/UserInterfaceState.xcuserstate
    #	BalloonShooter/BalloonShooter/ViewController.swift

--- BalloonShooter/BalloonShooter/ViewController.swift
+++ BalloonShooter/BalloonShooter/ViewController.swift
@@ -5,10 +5,15 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, SCNSceneRendererDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     var transform : matrix_float4x4!
+<<<<<<< HEAD
+=======
+    var targetCreationTime:TimeInterval = 0
+    let egg = SCNScene(named: "Egg.scn")!
+>>>>>>> parent of fc68387... hope this works
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +44,19 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         //var cupnode=SCNNode()
         sceneView.scene.rootNode.addChildNode(cupnode)
 
+<<<<<<< HEAD
         game()
+=======
+
+        var i=0
+
+        //while i==0{
+         //   transform = sceneView.session.currentFrame?.camera.transform
+         //   cupnode.position=SCNVector3Make((transform?.columns.3.x)!, (transform?.columns.3.y)!, (transform?.columns.3.z)!)
+        //    sceneView.scene.rootNode.addChildNode(cupnode)
+        //}
+
+>>>>>>> parent of fc68387... hope this works
     }
     func followCamera(){
             }
@@ -85,19 +102,42 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         let B: Int = 1
         let number = Int(arc4random_uniform(UInt32(B - A + 1))) + A
         Float32(number)
+<<<<<<< HEAD
         eggnode.position = SCNVector3(number, -1, -2)
+=======
+        eggnode.position = SCNVector3(number, -1, 10)
+>>>>>>> parent of fc68387... hope this works
         eggnode.scale = SCNVector3(0.09, 0.09, 0.09)
         sceneView.scene.rootNode.addChildNode(eggnode)
         eggnode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
         if eggnode.position.z <= -5 {
             eggnode.physicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.static, shape: nil)
+<<<<<<< HEAD
+=======
         }
     }
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if time > targetCreationTime {
+            createEgg()
+            targetCreationTime = time + 0.6
+>>>>>>> parent of fc68387... hope this works
+        }
+        cleanUp()
+    }
+<<<<<<< HEAD
     func game() {
         var a = 5
         while a >= 0 {
             createEgg()
             a -= 1
+=======
+    
+    func cleanUp() {
+        for node in egg.rootNode.childNodes {
+            if node.presentation.position.y < -5 {
+                node.removeFromParentNode()
+            }
+>>>>>>> parent of fc68387... hope this works
         }
     }
     
